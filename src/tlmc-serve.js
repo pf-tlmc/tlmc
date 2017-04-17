972dac2bc6e59269581016cee6040222dc940949
Markus Tran <thebluepillow@gmail.com>
2017-04-17 10:32:03 -0700

    check dir

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -2,15 +2,27 @@ const fs = require('fs')
 const path = require('path')
 const parse = require('./parse')
 
-const TLMC_PATH = process.env.TLMC_PATH || '/mnt/tlmc'
+const TLMC_PATH = process.argv[2] || process.env.TLMC_PATH || '/mnt/tlmc'
 
 console.log('Reading TLMC directory...')
 const directory = parse.ls(TLMC_PATH)
 const songs = parse.enumSongs(directory)
 
 console.log('Checking file paths...')
-for (const song of songs) {
-  fs.accessSync(path.join(TLMC_PATH, song), fs.constants.W_OK)
-}
+const failed = songs.filter(song => {
+  try {
+    fs.accessSync(path.join(TLMC_PATH, song), fs.constants.R_OK)
+    return false
+  }
+  catch (error) {
+    return true
+  }
+})
 
-console.log('All OK!')
+if (failed.length) {
+  console.log(`${failed.length} songs not found:`)
+  console.log(failed.join('\n'))
+}
+else {
+  console.log('All OK!')
+}
