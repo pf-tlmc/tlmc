3167aaa21102680ef6f770ddc90b471a18136cb6
Markus Tran <thebluepillow@gmail.com>
2017-04-17 13:04:36 -0700

    RegExp escape

--- src/parse.js
+++ src/parse.js
@@ -12,7 +12,11 @@ const SANITIZE_MAP = {
 
 function sanitize(fileName) {
   for (const key in SANITIZE_MAP) {
-    fileName = fileName.replace(new RegExp(key, 'g'), SANITIZE_MAP[key])
+    // http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
+    fileName = fileName.replace(
+      new RegExp(key.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'),
+      SANITIZE_MAP[key]
+    )
   }
   return fileName
 }
