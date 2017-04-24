ebcda79b5be7a43ffaaf0e79afdf38821a8886a8
Markus Tran <thebluepillow@gmail.com>
2017-04-24 13:45:39 -0700

    update eslint-config-pf

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const path = require('path')
-const cueParser = require('cue-parser')
+const fs = require('fs');
+const path = require('path');
+const cueParser = require('cue-parser');
 
 const SANITIZE_MAP = {
   '/': '-',
@@ -12,72 +12,72 @@ const SANITIZE_MAP = {
   '<': '_',
   '>': '_',
   '\t': '_'
-}
+};
 for (const key in SANITIZE_MAP) {
   // http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
   SANITIZE_MAP[key] = [
     new RegExp(key.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'),
     SANITIZE_MAP[key]
-  ]
+  ];
 }
 
 function sanitize(title) {
   for (const key in SANITIZE_MAP) {
-    title = title.replace(...SANITIZE_MAP[key])
+    title = title.replace(...SANITIZE_MAP[key]);
   }
-  return title
+  return title;
 }
 
 function ls(dirPath, dirName = path.sep) {
-  const files = []
-  const cues = []
+  const files = [];
+  const cues = [];
 
   fs.readdirSync(dirPath).forEach(fileName => {
-    const filePath = path.join(dirPath, fileName)
-    const fileStat = fs.statSync(filePath)
+    const filePath = path.join(dirPath, fileName);
+    const fileStat = fs.statSync(filePath);
 
     if (fileStat.isDirectory()) {
-      files.push(ls(filePath, fileName))
+      files.push(ls(filePath, fileName));
     }
 
     else if (fileStat.isFile()) {
       if (path.extname(fileName) === '.cue') {
         try {
-          cues.push(cueParser.parse(filePath))
+          cues.push(cueParser.parse(filePath));
         }
         catch (error) {
-          throw new TypeError(`could not parse ${filePath}`)
+          throw new TypeError(`could not parse ${filePath}`);
         }
       }
-      files.push(fileName)
+      files.push(fileName);
     }
 
     else {
-      throw new TypeError(`unknown file type found: ${filePath}`)
+      throw new TypeError(`unknown file type found: ${filePath}`);
     }
-  })
+  });
 
   return cues.length
     ? {name: dirName, files, cues}
-    : {name: dirName, files}
+    : {name: dirName, files};
 }
 
 function formatTrack(track, file) {
   const title = track.title
     ? sanitize(track.title)
-    : path.parse(file.name).name
-  return `${('00' + track.number).slice(-2)}. ${title}.mp3`
+    : path.parse(file.name).name;
+  return `${('00' + track.number).slice(-2)}. ${title}.mp3`;
 }
 
 function enumSongs(dir) {
-  const songs = []
+  const songs = [];
 
   function _enum(dir, dirPath) {
     if (dir.cues) {
       for (const cue of dir.cues) {
         for (const file of cue.files) {
           for (const track of file.tracks) {
-            songs.push(path.join(dirPath, formatTrack(track, file)))
+            songs.push(path.join(dirPath, formatTrack(track, file)));
           }
         }
       }
@@ -85,13 +85,13 @@ function enumSongs(dir) {
 
     for (const file of dir.files) {
       if (typeof file === 'object') {
-        _enum(file, path.join(dirPath, file.name))
+        _enum(file, path.join(dirPath, file.name));
       }
     }
   }
 
-  _enum(dir, '')
-  return songs
+  _enum(dir, '');
+  return songs;
 }
 
-module.exports = {ls, enumSongs}
+module.exports = {ls, enumSongs};
--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -1,45 +1,45 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const parse = require('./parse')
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const parse = require('./parse');
 
-const PORT = process.argv[2] || process.env.TLMC_PORT || 80
-const TLMC_PATH = process.argv[3] || process.env.TLMC_PATH || '/mnt/tlmc'
+const PORT = process.argv[2] || process.env.TLMC_PORT || 80;
+const TLMC_PATH = process.argv[3] || process.env.TLMC_PATH || '/mnt/TouhouBox/tlmc';
 
 /* eslint-disable no-console */
-console.log('Reading TLMC directory...')
-const directory = parse.ls(TLMC_PATH)
-const songs = parse.enumSongs(directory)
+console.log('Reading TLMC directory...');
+const directory = parse.ls(TLMC_PATH);
+const songs = parse.enumSongs(directory);
 
-console.log('Checking file paths...')
+console.log('Checking file paths...');
 const failed = songs.filter(song => {
   try {
-    fs.accessSync(path.join(TLMC_PATH, song), fs.constants.R_OK)
-    return false
+    fs.accessSync(path.join(TLMC_PATH, song), fs.constants.R_OK);
+    return false;
   }
   catch (error) {
-    return true
+    return true;
   }
-})
+});
 
 if (failed.length) {
-  console.log(`${failed.length} songs not found:`)
-  console.log(failed.join('\n'))
+  console.log(`${failed.length} songs not found:`);
+  console.log(failed.join('\n'));
 }
 else {
-  console.log('All OK!')
+  console.log('All OK!');
 }
 
-const app = express()
-const directoryString = JSON.stringify(directory)
+const app = express();
+const directoryString = JSON.stringify(directory);
 
-app.use('/tlmc', express.static(TLMC_PATH))
+app.use('/tlmc', express.static(TLMC_PATH));
 app.get('/ls', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(directoryString)
-})
+  res.setHeader('Content-Type', 'application/json');
+  res.send(directoryString);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
 /* eslint-enable no-console */
