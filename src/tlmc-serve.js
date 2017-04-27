43339d858fc929d4aba5635fd770288ffd4c1dab
Markus Tran <thebluepillow@gmail.com>
2017-04-27 13:46:49 -0700

    custom serialization

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -1,70 +1,58 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const compression = require('compression');
-
-const parse = require('./parse');
+// const compression = require('compression');
+const ls = require('ls-serialize');
 
 const PORT = process.argv[2] || process.env.TLMC_PORT || 80;
 const TLMC_PATH = process.argv[3] || process.env.TLMC_PATH || '/mnt/TouhouBox/tlmc';
-const CACHE_PATH = path.join(__dirname, 'ls.cache.json');
+const LS_CACHE_PATH = path.join(__dirname, 'ls.cache');
 
 /* eslint-disable no-console */
-let cacheExists;
 try {
-  fs.accessSync(CACHE_PATH);
-  cacheExists = true;
+  fs.accessSync(LS_CACHE_PATH);
+  console.log('ls-cache found. Reading from cache.');
 }
+
 catch (err) {
-  cacheExists = false;
+  console.log('No ls-cache found. Creating directory structure...');
+  const directoryString = ls.serialize(TLMC_PATH);
+  fs.writeFileSync(LS_CACHE_PATH, directoryString);
 }
 
-let songs;
-
-if (!cacheExists) {
-  console.log('No cache found. Creating directory structure...');
-  const directory = parse.ls(TLMC_PATH);
-  fs.writeFileSync(CACHE_PATH, JSON.stringify(directory));
-  songs = parse.enumSongs(directory);
-}
-else {
-  console.log('Cache found. Reading from cache.');
-  songs = parse.enumSongs(JSON.parse(fs.readFileSync(CACHE_PATH)));
-}
-
-console.log('Checking file paths...');
-let failed = songs.filter(song => {
-  try {
-    fs.accessSync(path.join(TLMC_PATH, song), fs.constants.R_OK);
-    return false;
-  }
-  catch (error) {
-    return true;
-  }
-});
-
-if (failed.length) {
-  console.log(`${failed.length} songs not found:`);
-  console.log(failed.join('\n'));
-}
-else {
-  console.log('All OK!');
-}
-
-songs = undefined;
-failed = undefined;
+// console.log('Checking file paths...');
+// let failed = songs.filter(song => {
+//   try {
+//     fs.accessSync(path.join(TLMC_PATH, song), fs.constants.R_OK);
+//     return false;
+//   }
+//   catch (error) {
+//     return true;
+//   }
+// });
+//
+// if (failed.length) {
+//   console.log(`${failed.length} songs not found:`);
+//   console.log(failed.join('\n'));
+// }
+// else {
+//   console.log('All OK!');
+// }
+//
+// songs = undefined;
+// failed = undefined;
 
 const app = express();
 
-app.use(compression({level: 9}));
+// app.use(compression({level: 9}));
 
 app.use('/tlmc', express.static(TLMC_PATH));
 
 app.get('/ls', (req, res) => {
-  res.sendFile(CACHE_PATH);
+  res.sendFile(LS_CACHE_PATH);
 });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-/* eslint-enable no-console */
--- src/tlmc.js
+++ src/tlmc.js
@@ -1,28 +1,25 @@
-const path = require('path');
+/* eslint-disable no-console */
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const request = require('request');
 const compression = require('compression');
 const ProgressBar = require('progress');
-const parse = require('./parse');
+// const parse = require('./parse');
 
 const PORT = process.argv[2] || process.env.TLMC_PORT || 80;
-const TLMC_SERVE_URL = process.argv[3] || process.env.TLMC_SERVE_URL || 'http://192.168.1.62:8080';
-const WEBSITE_PATH = path.join(__dirname, 'website', 'index.html');
-const PUBLIC_PATH = path.join(__dirname, 'website', 'public');
-const CACHE_PATH = path.join(__dirname, 'ls.cache.json');
+const TLMC_SERVE_URL = process.argv[3] || process.env.TLMC_SERVE_URL || 'http://home.pf-n.co';
+// const WEBSITE_PATH = path.join(__dirname, 'website', 'index.html');
+// const PUBLIC_PATH = path.join(__dirname, 'website', 'public');
+const LS_CACHE_PATH = path.join(__dirname, 'ls.cache');
 
-/* eslint-disable no-console */
-let cacheExists;
 try {
-  fs.accessSync(CACHE_PATH);
-  cacheExists = true;
-}
-catch (err) {
-  cacheExists = false;
+  fs.accessSync(LS_CACHE_PATH);
+  console.log('Cache found. Reading from cache.');
+  createServer();
 }
 
-if (!cacheExists) {
+catch (err) {
   console.log('No cache found. Fetching directory structure...');
   request.get(`${TLMC_SERVE_URL}/ls`)
     .on('response', res => {
@@ -49,36 +46,31 @@ if (!cacheExists) {
       console.log(`Could not fetch \`${TLMC_SERVE_URL}/ls\`. Please make sure tlmc-serve is running, or try again.`);
       console.log(err);
     })
-    .pipe(fs.createWriteStream(CACHE_PATH));
-}
-else {
-  console.log('Cache found. Reading from cache.');
-  createServer();
+    .pipe(fs.createWriteStream(LS_CACHE_PATH));
 }
 
 function createServer() {
-  const directory = JSON.parse(fs.readFileSync(CACHE_PATH));
-  const songs = parse.enumSongs(directory);
-  const directoryString = JSON.stringify(directory);
+  // const directory = JSON.parse(fs.readFileSync(LS_CACHE_PATH));
+  // const songs = parse.enumSongs(directory);
+  const directoryString = fs.readFileSync(LS_CACHE_PATH);
   const app = express();
 
   app.use(compression());
 
   app.get('/tlmc/ls', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
     res.send(directoryString);
   });
 
-  app.get('/tlmc/id/:id', (req, res) => {
-    const index = +req.params.id - 1;
-    const songPath = songs[index];
-    if (!songPath) {
-      return res.sendStatus(404);
-    }
-
-    const cleanPath = songPath.split('/').map(encodeURIComponent).join('/');
-    request.get(`${TLMC_SERVE_URL}/tlmc/${cleanPath}`).pipe(res);
-  });
+  // app.get('/tlmc/id/:id', (req, res) => {
+  //   const index = +req.params.id - 1;
+  //   const songPath = songs[index];
+  //   if (!songPath) {
+  //     return res.sendStatus(404);
+  //   }
+  //
+  //   const cleanPath = songPath.split('/').map(encodeURIComponent).join('/');
+  //   request.get(`${TLMC_SERVE_URL}/tlmc/${cleanPath}`).pipe(res);
+  // });
 
   app.get(/^\/tlmc\/(.+)/, (req, res) => {
     const [tlmcPath] = req.params;
@@ -86,14 +78,13 @@ function createServer() {
     request.get(`${TLMC_SERVE_URL}/tlmc/${tlmcPath}`).pipe(res);
   });
 
-  app.use('/public', express.static(PUBLIC_PATH));
-
-  app.get('*', (req, res) => {
-    res.sendFile(WEBSITE_PATH);
-  });
+  // app.use('/public', express.static(PUBLIC_PATH));
+  //
+  // app.get('*', (req, res) => {
+  //   res.sendFile(WEBSITE_PATH);
+  // });
 
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
 }
-/* eslint-enable no-console */
