dede0adf571850273f6a199d78d0323403a9ac40
Markus Tran <thebluepillow@gmail.com>
2017-05-02 14:22:57 -0700

    fix csv parse

--- src/tlmc-serve.js
+++ src/tlmc-serve.js
@@ -13,38 +13,36 @@ const TLMC_PATH = process.argv[3] || process.env.TLMC_PATH || '/mnt/TouhouBox/tl
 const LS_CACHE_PATH = path.join(__dirname, 'ls.cache');
 const CUE_CACHE_PATH = path.join(__dirname, 'cue.cache');
 
-// Initialize `ls.cache`
-try {
-  fs.accessSync(LS_CACHE_PATH);
-  console.log('ls-cache found. Using cache.');
-}
-
-catch (err) {
-  console.log('No ls-cache found. Creating directory structure...');
-  const directoryString = ls.serialize(TLMC_PATH);
-  fs.writeFileSync(LS_CACHE_PATH, directoryString);
-}
-
-// Initialize `cue.cache`
-try {
-  fs.accessSync(CUE_CACHE_PATH);
-  console.log('cue-cache found. Using cache.');
-}
-
-catch (err) {
-  console.log('No cue-cache found. Reading directory structure...');
-  const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString());
-  const cues = readCues(directory, TLMC_PATH);
-  const songs = parseCues(cues);
-  const songsString = csvStringify(songs, {header: true});
-  // const cuesString = JSON.stringify(cues);
-  fs.writeFileSync(CUE_CACHE_PATH, songsString);
-}
-
 {
+  // Initialize `ls.cache`
+  try {
+    fs.accessSync(LS_CACHE_PATH);
+    console.log('ls-cache found. Using cache.');
+  }
+
+  catch (err) {
+    console.log('No ls-cache found. Creating directory structure...');
+    const directoryString = ls.serialize(TLMC_PATH);
+    fs.writeFileSync(LS_CACHE_PATH, directoryString);
+  }
+
+  // Initialize `cue.cache`
+  try {
+    fs.accessSync(CUE_CACHE_PATH);
+    console.log('cue-cache found. Using cache.');
+  }
+
+  catch (err) {
+    console.log('No cue-cache found. Reading directory structure...');
+    const directory = ls.deserialize(fs.readFileSync(LS_CACHE_PATH).toString());
+    const cues = readCues(directory, TLMC_PATH);
+    const songs = parseCues(cues);
+    const songsString = csvStringify(songs, {header: true});
+    fs.writeFileSync(CUE_CACHE_PATH, songsString);
+  }
+
   console.log('Checking file paths...');
-  const songs = csvParse(fs.readFileSync(CUE_CACHE_PATH).toString());
-  // const cues = JSON.parse(fs.readFileSync(CUE_CACHE_PATH).toString());
+  const songs = csvParse(fs.readFileSync(CUE_CACHE_PATH).toString(), {columns: true});
   let failed = songs.filter(song => {
     try {
       fs.accessSync(path.join(TLMC_PATH, song.path));
@@ -72,6 +70,9 @@ app.use('/tlmc', express.static(TLMC_PATH));
 
 app.get('/ls', (req, res) => {
   res.sendFile(LS_CACHE_PATH);
+});
+app.get('/cue', (req, res) => {
+  res.sendFile(CUE_CACHE_PATH);
 });
 
 app.listen(PORT, () => {
