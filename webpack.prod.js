2840e886621456c325391001e116b524e59d0842
Markus Tran <thebluepillow@gmail.com>
2017-05-03 18:40:17 -0700

    split prod and dev

---
+++ webpack.prod.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'website', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'src', 'website', 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  node: {
+    fs: 'empty',
+    path: true // See src/website/path.js for full patch
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react'],
+          cacheDirectory: true
+        }
+      }
+    }, {
+      test: /\.s?css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: 'css-loader',
+          options: {
+            minimize: true
+          }
+        }, {
+          loader: 'sass-loader'
+        }, {
+          loader: 'import-glob-loader'
+        }]
+      })
+    }, {
+      test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+          name: '/fonts/[name].[ext]'
+        }
+      }]
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '/fonts/[name].[ext]'
+        }
+      }]
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      comments: false
+    })
+  ]
+};
