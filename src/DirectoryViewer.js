d459d089d342a304646169aab21497e04d20bed1
Markus Tran <thebluepillow@gmail.com>
2020-07-19 08:35:05 -0700

    virtualize lists

--- src/DirectoryViewer.js
+++ src/DirectoryViewer.js
@@ -1,24 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import List from '@material-ui/core/List'
+import { FixedSizeList as List } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
 import Link from './Link'
 import FileIcon from './FileIcon'
 
 const DirectoryViewer = ({ directory }) => {
+  const files = [...directory.files]
+
+  function renderRow ({ index, style }) {
+    const file = files[index]
+    return (
+      <Link href='/tlmc/[...tlmc_path]' as={'/tlmc' + file.path}>
+        <ListItem button style={style}>
+          <ListItemIcon><FileIcon file={file} /></ListItemIcon>
+          <ListItemText><Typography noWrap>{file.base}</Typography></ListItemText>
+        </ListItem>
+      </Link>
+    )
+  }
+
   return (
-    <List dense>
-      {[...directory.files].map((file, index) =>
-        <Link key={index} href='/tlmc/[...tlmc_path]' as={'/tlmc' + file.path}>
-          <ListItem button>
-            <ListItemIcon><FileIcon file={file} /></ListItemIcon>
-            <ListItemText>{file.base}</ListItemText>
-          </ListItem>
-        </Link>
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          height={height}
+          width={width}
+          itemCount={files.length}
+          itemSize={42}
+          overscanCount={10}
+        >
+          {renderRow}
+        </List>
       )}
-    </List>
+    </AutoSizer>
   )
 }
 
