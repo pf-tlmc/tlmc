04b2e6e72986cf3e5398acaf2d81385d973fa199
Markus Tran <thebluepillow@gmail.com>
2020-07-19 01:02:00 -0700

    switch to http pipe

--- pages/api/ls.js
+++ pages/api/ls.js
@@ -1,14 +1,13 @@
-import fetch from 'isomorphic-fetch'
+import http from 'http'
 
 const LS_URL = 'http://home.pf-n.co:3000/ls'
 
 export default (req, res) => {
-  return fetch(LS_URL)
-    .then((fetchRes) => {
-      res.status(200).send(fetchRes.body)
-    })
-    .catch((error) => {
-      console.log(error)
-      res.status(500).send('Error')
-    })
+  return http.get(LS_URL, (httpRes) => httpRes.pipe(res))
+}
+
+export const config = {
+  api: {
+    externalResolver: true
+  }
 }
