814e94497dfa1d20bda409c93379c2c3d6cae2b9
Markus Tran <thebluepillow@gmail.com>
2020-07-22 14:59:55 -0700

    smooth search

--- src/search/SearchResults.js
+++ src/search/SearchResults.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { clearSearch } from '../redux/actions'
 import { makeStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import Typography from '@material-ui/core/Typography'
 import DirectoryViewer from '../viewers/DirectoryViewer'
-import getSearchResults from './get-search-results'
+import useSearch from './use-search'
 
 const useStyles = makeStyles((theme) => ({
   searchResults: {
     padding: theme.spacing(0, 0, 20),
     '& h5': {
       margin: theme.spacing(2, 0, 1, 2)
+    }
+  },
+  progress: {
+    position: 'fixed',
+    width: '100vw',
+    bottom: 0,
+    '& > div': {
+      transition: 'none'
     }
   }
 }))
@@ -21,19 +30,26 @@ const SearchResults = connect(
   { clearSearch }
 )(
   ({ ls, search, clearSearch }) => {
-    const { circles, albums, songs, other } = getSearchResults(search, ls)
+    const searchResults = useSearch(ls, search)
+
+    if (!searchResults) {
+      return null
+    }
+
+    const [{ circles, albums, songs, other }, progress] = searchResults
     const classes = useStyles()
 
     if (circles.length + albums.length + songs.length === 0) {
       return (
         <div className={classes.searchResults}>
-          <Typography variant='h5'>No results</Typography>
+          <Typography variant='h5'>{progress < 1 ? 'Searching...' : 'No results'}</Typography>
         </div>
       )
     }
 
     return (
       <div className={classes.searchResults}>
+        <LinearProgress variant='determinate' value={progress * 100} className={classes.progress} />
         {circles.length > 0 &&
           <>
             <Typography variant='h5' gutterBottom>Circles</Typography>
---
+++ src/search/use-search.js
@@ -0,0 +1,89 @@
+import { useState, useRef, useEffect } from 'react'
+
+const CHUNK_SIZE = 10
+const SEARCH_DELAY = 1
+
+function useSearch (ls, search) {
+  const searchStatus = useRef({
+    search,
+    files: [...ls.files],
+    index: 0,
+    timeout: null
+  })
+
+  const [searchResults, setSearchResults] = useState({ circles: [], albums: [], songs: [], other: [] })
+
+  useEffect(() => {
+    if (search !== searchStatus.current.search) {
+      searchStatus.current.search = search
+      searchStatus.current.index = CHUNK_SIZE
+      if (searchStatus.current.timeout) {
+        clearTimeout(searchStatus.current.timeout)
+        searchStatus.current.timeout = null
+      }
+      const searchResults = { circles: [], albums: [], songs: [], other: [] }
+      const { files } = searchStatus.current
+      for (let i = 0; i < CHUNK_SIZE && i < files.length; ++i) {
+        processNode(files[i], search, searchResults)
+      }
+      setSearchResults(searchResults)
+    } else {
+      const { files, index, timeout } = searchStatus.current
+      if (index < files.length && !timeout) {
+        searchStatus.current.timeout = setTimeout(() => {
+          for (let i = 0; i < CHUNK_SIZE && index + i < files.length; ++i) {
+            processNode(files[index + i], search, searchResults)
+          }
+          searchStatus.current.index += CHUNK_SIZE
+          searchStatus.current.timeout = null
+          setSearchResults({ ...searchResults })
+        }, SEARCH_DELAY)
+      }
+    }
+
+    return () => {
+      searchStatus.current.timeout && clearTimeout(searchStatus.current.timeout)
+    }
+  })
+
+  const { index, files } = searchStatus.current
+  return [searchResults, Math.min(index / files.length, 1)]
+}
+
+function getNodeType (node) {
+  if (node.isDirectory) {
+    if (node.parent.isRoot) {
+      return 'CIRCLE'
+    } else {
+      for (const file of node) {
+        if (file.ext.toLowerCase() === '.tta') {
+          return 'ALBUM'
+        }
+      }
+    }
+  } else {
+    if (node.ext.toLowerCase() === '.mp3') {
+      return 'SONG'
+    }
+  }
+
+  return 'OTHER'
+}
+
+function processNode (node, search, searchResults) {
+  if (node.base.toLowerCase().indexOf(search.toLowerCase()) > -1) {
+    switch (getNodeType(node)) {
+      case 'CIRCLE': searchResults.circles.push(node); break
+      case 'ALBUM': searchResults.albums.push(node); break
+      case 'SONG': searchResults.songs.push(node); break
+      case 'OTHER': searchResults.other.push(node); break
+    }
+  }
+  if (node.isDirectory) {
+    for (const file of node) {
+      processNode(file, search, searchResults)
+    }
+  }
+}
+
+export default useSearch
