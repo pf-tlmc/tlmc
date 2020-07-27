4af4fb81ba0d7b9ea9a0a1b0b0b5df7c98ff4eb9
Markus Tran <thebluepillow@gmail.com>
2020-07-26 19:57:27 -0700

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,17 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' />
+          <script async src='https://www.googletagmanager.com/gtag/js?id=UA-137452548-2' />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-137452548-2');
+              `
+            }}
+          />
         </Head>
         <body>
           <Main />
