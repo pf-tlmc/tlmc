0e9919fea7f1483dd683da48ea4071a66fa10b17
Markus Tran <thebluepillow@gmail.com>
2020-07-26 19:16:28 -0700

    about

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Page from '../src/Page'
+import Container from '../src/Container'
 
 const About = () => {
   return (
     <Page>
-      <Typography variant='h2'>About</Typography>
-      <Typography paragraph>This is the TLMC</Typography>
+      <Container>
+        <Typography variant='h4' gutterBottom>About</Typography>
+        <Typography paragraph>This is the TLMC</Typography>
+      </Container>
     </Page>
   )
 }
--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
 import MuiContainer from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(1)
+  }
+}))
+
 const Container = ({ title, children }) => {
+  const classes = useStyles()
+
   return (
-    <MuiContainer>
+    <MuiContainer className={classes.container}>
       {title && <Typography variant='h5' gutterBottom>{title}</Typography>}
       {children}
     </MuiContainer>
