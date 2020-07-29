71b62cc62d7c558c85bf14f171f0508083ea7795
Markus Tran <thebluepillow@gmail.com>
2020-07-28 17:14:57 -0700

    add coverimagemap

--- src/CoverImage.js
+++ src/CoverImage.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { isImage } from './utils'
+import COVER_IMAGE_MAP from './cover-image-map'
 
 const COVER_REGEX = /^(image|img|jacket).*0*1\.(jpe?g|png|gif)$/i
 const COVER_FOLDER_REGEX = /^(images?|covers?|scans?)$/i
 
 export function findCoverImage (directory) {
-  // First try images in current folder
+  // First try using the map
+  const path = COVER_IMAGE_MAP[directory.path]
+  if (path) {
+    let node = directory
+    for (const edge of path.split('/')) {
+      if (edge === '..') {
+        node = node.parent
+      } else {
+        node = node.get(edge)
+      }
+    }
+    return node
+  }
+
+  // Then try images in current folder
   for (const file of directory) {
     if (file.isFile && COVER_REGEX.test(file.base)) {
       return file
