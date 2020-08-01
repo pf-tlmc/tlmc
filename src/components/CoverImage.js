62c211e94a3780ec04a2b819914f184b3cd4ab04
Markus Tran <thebluepillow@gmail.com>
2020-08-01 16:15:16 -0700

    add cover image loading placeholder

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { File } from 'ls-serialize/src/structures'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
 import { urlEncode, isImage } from '../utils'
 import COVER_IMAGE_MAP from '../cover-image-map'
 
@@ -8,7 +10,15 @@ const COVER_REGEX = /^(image|img|jacket).*0*1\.(jpe?g|png|gif)$/i
 const COVER_FOLDER_REGEX = /^(images?|covers?|scans?)$/i
 const DEFAULT_IMAGE = '/images/album-placeholder.png'
 
-export function findCoverImage (cueFile) {
+const useStyles = makeStyles((theme) => ({
+  coverImage: {
+    width: ({ size }) => size,
+    height: ({ size }) => size,
+    backgroundColor: theme.palette.action.hover
+  }
+}))
+
+function findCoverImage (cueFile) {
   const directory = cueFile.isDirectory ? cueFile : cueFile.parent
 
   // First try using the map
@@ -56,10 +66,16 @@ export function findCoverImage (cueFile) {
   }
 }
 
-const CoverImage = ({ cueFile, size, ...props }) => {
+const CoverImage = ({ cueFile, size = 200, imageProps, className, ...props }) => {
+  const classes = useStyles({ size })
   const coverImage = cueFile && findCoverImage(cueFile)
   const src = coverImage ? `/api/tlmc${urlEncode(coverImage.path)}?size=thumbnail` : DEFAULT_IMAGE
-  return <img src={src} width={size} height={size} {...props} />
+
+  return (
+    <div className={clsx(classes.coverImage, className)} {...props}>
+      <img src={src} width={size} height={size} {...imageProps} />
+    </div>
+  )
 }
 
 CoverImage.propTypes = {
